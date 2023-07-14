719706e67c8c7797a09922cfb8c0f6d1abe2ab6b
guga56 <gustavogregorio086@gmail.com>
2023-07-14 11:34:24 -0300

    criando projeto com microservviços para estudar aprender a estrutura melhor

---
+++ SistemaTarefas.Servico/Servico/Interface/ITarefa.cs
@@ -0,0 +1,16 @@
+﻿using SsitemaTarefas.Dominio.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaTarefas.Servico.Servico.Interface
+{
+    public interface ITarefa
+    {
+        Task<Tarefa> Adicionar(Tarefa tarefa);
+
+
+    }
+}
