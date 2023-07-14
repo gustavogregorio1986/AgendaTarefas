17bc119eb624337bb9d242913b89bd5140b7108a
guga56 <gustavogregorio086@gmail.com>
2023-07-14 12:10:02 -0300

    projeto completo de API

---
+++ SistemaTarefas/Controllers/TarefaController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SistemaTarefas.Data.Repositorio;
+using SsitemaTarefas.Dominio.Dominio;
+
+namespace SistemaTarefas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TarefaController : ControllerBase
+    {
+        private readonly TarefaRepositorio _tarefaRepositorio;
+
+        public TarefaController(TarefaRepositorio tarefaRepositorio)
+        {
+            _tarefaRepositorio = tarefaRepositorio;
+        }
+
+        [HttpGet]
+        public Task<List<Tarefa>> ListarTodasTarefas()
+        {
+            return _tarefaRepositorio.BuscarTodasTarefas();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Tarefa>> BuscarPorId(int id)
+        {
+            var tarefa = await _tarefaRepositorio.BuscarPorId(id);
+
+            return Ok(tarefa);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Tarefa>> Adicionar([FromBody]Tarefa tarefa)
+        {
+            var tarefaModel = _tarefaRepositorio.Adicionar(tarefa);
+
+            return Ok(tarefaModel);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Tarefa>> Atualizar([FromBody] Tarefa tarefa)
+        {
+            var tarefaModel = _tarefaRepositorio.Adicionar(tarefa);
+
+            return Ok(tarefaModel);
+        }
+
+    }
+}
